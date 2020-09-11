337ec96ef4c6b072f2025c8f6762bcfec992c592
jcwrightson <jcwrightson@gmail.com>
2020-09-11 14:49:41 +0100

    dynamic width

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@
 import Snake from './snake'
 import '../style.scss'
 
+const calcWidth = () => {
+  const w = Math.floor(window.innerWidth / 20 - window.innerWidth * 0.01)
+  return w
+}
+
+const calcHeight = () => {
+  const h = Math.floor(window.innerHeight / 25 - window.innerHeight * 0.01)
+  return h
+}
+
 window.addEventListener('DOMContentLoaded', () => {
   const game = Snake()
 
@@ -10,7 +20,10 @@ window.addEventListener('DOMContentLoaded', () => {
     sounds: false,
     startFPS: 10,
     scaleFactor: 15,
-    gameHeight: 30,
-    gameWidth: 60,
+    gameHeight: calcHeight(),
+    gameWidth: calcWidth() > 60 ? 60 : calcWidth(),
   })
+
+  let gamePixWidth = document.getElementById('snake').clientWidth
+  document.querySelector('main.game').style.width = gamePixWidth + 'px'
 })
